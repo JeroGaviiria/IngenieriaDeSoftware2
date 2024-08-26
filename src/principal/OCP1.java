6c753b51b4e06b9dfdb026488788586124056e6e
JeroGaviiria <jeronimo.gaviriar@autonoma.edu.co>
2024-08-26 14:41:38 -0500

    OCP1

---
+++ src/principal/OCP1.java
@@ -0,0 +1,33 @@
+package principal; 
+
+interface Envio {
+    double calcularCosto(double peso, double distancia);
+}
+
+class EnvioEstandar implements Envio {
+    @Override
+    public double calcularCosto(double peso, double distancia) {
+        return peso * 0.5 + distancia * 0.1;
+    }
+}
+
+class EnvioExpress implements Envio {
+    @Override
+    public double calcularCosto(double peso, double distancia) {
+        return peso * 1.0 + distancia * 0.2;
+    }
+}
+
+public class OCP1 {
+    public static void main(String[] args) {
+        Envio envioEstandar = new EnvioEstandar();
+        Envio envioExpress = new EnvioExpress();
+
+        double peso = 10.0;
+        double distancia = 50.0;
+
+        System.out.println("Costo Envío Estándar: " + envioEstandar.calcularCosto(peso, distancia));
+        System.out.println("Costo Envío Express: " + envioExpress.calcularCosto(peso, distancia));
+    }
+}
+
